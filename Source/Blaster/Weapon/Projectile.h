c0c23b08cefdcbd68161ee3413e63de0b8ed6ee4
SpringGodCuisine <77707704+SpringGodCuisine@users.noreply.github.com>
2024-09-13 11:14:27 +0800

    命中目标

--- Source/Blaster/Weapon/Projectile.h
+++ Source/Blaster/Weapon/Projectile.h
@@ -17,8 +17,12 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
 public:	
 	virtual void Tick(float DeltaTime) override;
+	virtual void Destroyed() override;
 
 private:
 
@@ -31,4 +35,10 @@ private:
 	UPROPERTY(EditAnyWhere)
 	class UParticleSystem* Tracer;
 	class UParticleSystemComponent* TracerComponent;
+
+	UPROPERTY(EditAnyWhere)
+	UParticleSystem* ImpactParticles;
+
+	UPROPERTY(EditAnyWhere)
+	class USoundCue* ImpactSound;
 };
