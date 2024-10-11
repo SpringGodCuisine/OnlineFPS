e570e039adb86ac8baadd6b8e34e321b5dcd162e
SpringGodCuisine <77707704+SpringGodCuisine@users.noreply.github.com>
2024-10-11 18:45:33 +0800

    火箭筒

--- Source/Blaster/BlasterComponents/CombatComponent.h
+++ Source/Blaster/BlasterComponents/CombatComponent.h
@@ -135,6 +135,9 @@ private:
 	UPROPERTY(EditAnywhere)
 	int32 StartingARAmmo = 30.f;
 
+	UPROPERTY(EditAnywhere)
+	int32 StartingRocketAmmo = 0.f;
+
 	void InitializeCarriedAmmo();
 
 	UPROPERTY(ReplicatedUsing = OnRep_CombatState)
